a22b0f50be2111dce4ae5e69fe6f71b11257ee09
drunnals313 <davidrunnals@gmail.com>
2018-06-24 16:06:41 -0400

    updated js for handlebars

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -21,59 +21,23 @@ $(function () {
         );
     });
 
-    // $(".create-form").on("submit", function (event) {
-    //     console.log(`Attempting to create via the public/assets/js/burger.js`);
-    //     event.preventDefault();
-
-    //     var newBurger = {
-    //         name: $("#bu").val().trim(),
-    //     };
-
-    //     $.ajax("/api/burgers", {
-    //         type: "POST",
-    //         data: newBurger
-    //     }).then(
-    //         function () {
-    //             console.log("Added new burger");
-    //             location.reload();
-    //         }
-    //     );
-    // });
-
-    ////////////////////////////////////////////
-    $(".create-form").on("submit", function (event) {
+ 
+    $(".create-update-form").on("submit", function (event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
-        var newBurger = {
-            name: $("#bu").val().trim(),
+        var submittedBurger = {
+            name: $("#djr").val().trim(),
         };
 
         // Send the POST request.
         $.ajax("./api/burgers", {
             type: "POST",
-            data: newBurger
+            data: submittedBurger
         }).then(
             function () {
-                console.log("Added burger");
+                console.log("Added new burger");
                 location.reload();
             }
         );
     });
-    ///////////////////////////////////////////
-
-    $(".delete-burger").on("click", function (event) {
-        var id = $(this).data("id");
-
-        // Send the DELETE request.
-        $.ajax("./api/burgers/" + id, {
-            type: "DELETE"
-        }).then(
-            function () {
-                console.log("Ate burger", id);
-                // Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
-});
