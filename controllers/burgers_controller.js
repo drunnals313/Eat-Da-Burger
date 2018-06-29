122b0c847a597a609da48464d8ae483169d3a629
drunnals313 <davidrunnals@gmail.com>
2018-06-29 19:05:42 -0400

    updating ajax put function for devouring

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,4 +30,22 @@ router.put("/:id", function (req, res) {
     });
 });
 
+/* router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, result => {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+}); */
+
+
 module.exports = router;
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -24,22 +24,39 @@ $(function () {
     // not devouring the burgers correctly in deployed heroku version   almost there
     $(".devourNow").on("click", function (event) {
         var id = $(this).data("id");
-        var eatNow = $(this).data("eatburger");
+        /* var eatNow = $(this).data("eatburger");
 
         var orderOut = {
             devoured: eatNow
-        };
+        }; */
 
         // Send the PUT request.
         $.ajax("./api/burgers/" + id, {
             type: "PUT",
-            data: orderOut
+            data: true                    //orderOut
         }).then(
             function () {
-                console.log("changed to", eatNow);
+                console.log("changed to");   //eatNow
                 
                 location.reload();
             }
         );
     }); 
 });
+
+/* router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, result => {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+}); */
